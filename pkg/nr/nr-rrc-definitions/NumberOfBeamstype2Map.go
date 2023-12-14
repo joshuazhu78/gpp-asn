c99c17c33402b18ee2b76a92b74a11f6ccc3de36
Yuan Zhu <yuan_zhu123@hotmail.com>
2023-12-14 15:50:13 +0800

    add L and P map

---
+++ pkg/nr/nr-rrc-definitions/NumberOfBeamstype2Map.go
@@ -0,0 +1,7 @@
+package nrrrcdefinitions
+
+var NumberOfBeamstype2Map = map[NumberOfBeamstype2]int{
+	NumberOfBeamstype2_NUMBER_OF_BEAMSTYPE2_TWO:   2,
+	NumberOfBeamstype2_NUMBER_OF_BEAMSTYPE2_THREE: 3,
+	NumberOfBeamstype2_NUMBER_OF_BEAMSTYPE2_FOUR:  4,
+}
---
+++ pkg/nr/nr-rrc-definitions/PhaseAlphabetSizetype2Map.go
@@ -0,0 +1,6 @@
+package nrrrcdefinitions
+
+var PhaseAlphabetSizetype2Map = map[PhaseAlphabetSizetype2]int{
+	PhaseAlphabetSizetype2_PHASE_ALPHABET_SIZETYPE2_N4: 4,
+	PhaseAlphabetSizetype2_PHASE_ALPHABET_SIZETYPE2_N8: 8,
+}
